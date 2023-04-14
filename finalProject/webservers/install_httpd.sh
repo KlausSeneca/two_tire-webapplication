1256f54a9ea2810c33953cc5730e9f0504e538fd
EC2 Default User <ec2-user@ip-172-31-77-49.ec2.internal>
2023-04-14 17:09:17 +0000

    Webserver modification

--- finalProject/webservers/install_httpd.sh
+++ finalProject/webservers/install_httpd.sh
@@ -2,6 +2,6 @@
 yum -y update
 yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h1>Welcome to ACS730 Project! This is Group9_Final_Project My private IP is $myip</h1><br>Built by Terraform!" >  /var/www/html/index.html
+echo "<h1> This is Group9_Final_Project </h1> <br>  <img src="https://testgr9project.s3.amazonaws.com/Interesting.jpg" alt="Girl in a jacket" width="500" height="600">  <br>Built by Terraform!" >  /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
