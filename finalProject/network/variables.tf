985c2dbb6514d8591b899379d8c0103ce47aeddf
aliya <ec2-user@ip-172-31-47-162.ec2.internal>
2023-04-18 20:56:48 +0000

    Modified

--- finalProject/network/variables.tf
+++ finalProject/network/variables.tf
@@ -25,7 +25,7 @@ variable "private_subnet_cidrs" {
 # Name prefix
 variable "prefix" {
   type        = string
-  default     = "FinalProject"
+  default     = "Final-Project"
   description = "Name prefix"
 }
 
