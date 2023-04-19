3015ef951c6d0811add65d5b76e6ac8de0c71127
KlausSeneca <kcepani@myseneca.ca>
2023-04-19 17:38:26 +0000

    test

--- finalProject/webservers/main.tf
+++ finalProject/webservers/main.tf
@@ -18,7 +18,7 @@ data "aws_ami" "latest_amazon_linux" {
 data "terraform_remote_state" "network" { 
   backend = "s3"
   config = {
-    bucket = "projectgroup9" // Bucket from where to GET Terraform State
+    bucket = "finalprojectgr9" // Bucket from where to GET Terraform State
     key    = "final-net/terraform.tfstate" // Object name in the bucket to GET Terraform State
     region = "us-east-1"                            // Region where bucket created
   }
