8e4515cde472fdfd43b2c58e041fb6ac45b7aca1
aliya <ec2-user@ip-172-31-47-162.ec2.internal>
2023-04-19 03:31:53 +0000

    Addition

--- finalProject/webservers/install_httpd.sh
+++ finalProject/webservers/install_httpd.sh
@@ -2,6 +2,6 @@
 yum -y update
 yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h1> This is Group9_Final_Project </h1> <br>  <img src="https://testgr9project.s3.amazonaws.com/Interesting.jpg" alt="Seneca" width="90%" height="100%">  " >  /var/www/html/index.html
+echo "<h1> This is Group 9. We are presenting the Final Project. </h1> <br>  <img src="https://testgr9project.s3.amazonaws.com/Interesting.jpg" alt="Seneca" width="90%" height="100%">  " >  /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
