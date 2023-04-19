57e30ccb8dc4d572ac19a80cb28b1cd61c07c080
KlausSeneca <kcepani@myseneca.ca>
2023-04-19 02:26:36 +0000

    Test action

--- finalProject/network/variables.tf
+++ finalProject/network/variables.tf
@@ -8,7 +8,7 @@ variable "default_tags" {
   description = "AWS resource default tags"
 }
 # Provision public subnets in custom VPC
-variable "public_subnet_cidrs" {
+variable "Public_subnet_cidrs" {
   default     = ["10.1.1.0/24", "10.1.2.0/24","10.1.3.0/24", "10.1.4.0/24"]
   type        = list(string)
   description = "Public Subnet CIDRs"
